d81f82b86ba538736b405ab29f11b4f7c3849ea3
Altynai-Dev <altynai.imnl@gmail.com>
2024-08-01 14:30:48 +0600

    Add checkbox group an product card

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
-import { Title, Container, Categories, SortPopup } from "@/components/shared";
+import { Title, Container, TopBar, Filters } from "@/components/shared";
 
 export default function Home() {
   return (
-   <>
-    <Container className='mt-10'>
-      <Title text="Все пиццы" size="lg" className="font-extrabold" />
-    </Container>
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
 
-    <div className="sticky top-0 bg-white py-5 shadow-lg shadow-black/5">
-        <Container className="flex items-center justify-between ">
-          <Categories />
-          <SortPopup />
-        </Container>
-      </div>
-   </>
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">Cb</div>
+          </div>
+        </div>
+      </Container>
+    </>
   );
 }
