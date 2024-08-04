3b6519c7745a6f820331a87f9486c3942a66a185
Altynai-Dev <altynai.imnl@gmail.com>
2024-08-04 09:47:43 +0600

    Add models for db

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,10 +1,14 @@
 import { prisma } from "@/prisma/prisma-client";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 //the function will be called each time a request is made to /api/users
 export async function GET(){
     const users = await prisma.user.findMany()
-    return NextResponse.json({
-        users: ["user1", "user2", "user3"]
-    })
+    return NextResponse.json(users)
+}
+
+export async function POST(req: NextRequest) {
+    const data = await req.json();
+    const {password, ...user} = await prisma.user.create({data})
+    return NextResponse.json(user)
 }
